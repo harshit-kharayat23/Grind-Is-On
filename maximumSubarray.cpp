a1fe58ef62d9ec2e43d84d609a3b3754fad33f0c
harshit-kharayat23 <harshitkharyat15@gmail.com>
2025-07-07 21:14:06 +0530

    solved using kadanes algorithm in O(n) complexity

--- maximumSubarray.cpp
+++ maximumSubarray.cpp
@@ -2,6 +2,7 @@
 
 /*
 Brute Force
+TC:O(n^2)
 */
 #include<iostream>
 #include<vector>
@@ -19,5 +20,28 @@ public:
             }
         }
         return maxSum;
+    }
+};
+
+// Kadane's Algorithm 
+// TC:O(n)
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int maxSum=INT_MIN;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            maxSum=max(maxSum,sum);
+            if(sum<0){
+                sum=0;
+            }
+            
+
+        }
+        return maxSum;
+       
     }
 };
