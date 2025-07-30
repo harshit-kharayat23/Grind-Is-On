d6f5aefb7ee9f48aa6410723101d9f06849804ea
Harshit Kharayat <harshitkharayat15@gmail.com>
2025-07-30 08:09:16 +0530

    did ceil and floor question

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -26,3 +26,72 @@ public:
 
     }
 };
+
+//2. Ceil and  FLoor
+
+int findCeil(vector<int>&nums,int n,int target){
+
+	int l=0;
+	int r=n-1;
+	int ans=-1;
+	while(l<=r){
+
+		int mid=l+(r-l)/2;
+		if(nums[mid]>=target){
+			
+			ans=nums[mid];
+			r=mid-1;
+		}
+		else{
+			l=mid+1;
+			
+		}
+
+
+
+
+	}
+	return ans;
+
+
+
+}
+
+int findFloor(vector<int>&nums,int n,int target){
+
+	int l=0;
+	int r=n-1;
+	int ans=-1;
+	while(l<=r){
+
+		int mid=l+(r-l)/2;
+		if(nums[mid]<=target){
+			ans=nums[mid];
+			l=mid+1;
+		}
+		else{
+			r=mid-1;
+		}
+
+
+
+
+	}
+	return ans;
+
+
+
+}
+
+
+
+pair<int, int> getFloorAndCeil(vector<int> &a, int n, int x) {
+
+	int ceil=findCeil(a,n,x);
+	int floor=findFloor(a,n,x);
+
+	return {floor,ceil};
+
+
+
+}
