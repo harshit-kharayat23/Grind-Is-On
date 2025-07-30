0603e135feaf16d8e2ce75e5013942c804794959
Harshit Kharayat <harshitkharayat15@gmail.com>
2025-07-30 17:37:51 +0530

    Rotation

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -298,3 +298,46 @@ public:
     }
 };
 
+
+// Rotation
+
+#include<bits/stdc++.h>
+int findKRotation(vector<int> &arr){
+    
+    int n=arr.size();
+    int l=0;
+    int r=n-1;
+    int ans=INT_MAX;
+    int index=-1;
+    while(l<=r){
+
+        int mid=l+(r-l)/2;
+
+        // find the sorted half
+        if(arr[l]<=arr[mid]){
+            if(arr[l]<ans){
+                index=l;
+                ans=arr[l];
+                
+            }
+            l=mid+1;
+           
+         
+        }
+        else{
+            if(arr[mid]<ans){
+                ans=arr[mid];
+                index=mid;
+            }
+            r=mid-1;
+
+        }
+
+
+    }
+    return index;
+
+
+    
+}
+
