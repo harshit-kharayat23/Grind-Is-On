ac05b135480d9537da7b821cb62b0e81aab939e1
Harshit Kharayat <harshitkharayat15@gmail.com>
2025-07-30 08:18:51 +0530

    34. Find First and Last Position of Element in Sorted Array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -95,3 +95,83 @@ pair<int, int> getFloorAndCeil(vector<int> &a, int n, int x) {
 
 
 }
+
+//3 :34. Find First and Last Position of Element in Sorted Array
+
+class Solution {
+public:
+
+
+    int lowerBound(vector<int>&nums,int target){
+
+        int n=nums.size();
+        int l=0;
+        int r=n-1;
+        int ans=n;
+
+        while(l<=r){
+            int mid=(l+r)/2;
+
+            if(nums[mid]>=target){
+                ans=mid;
+                r=mid-1;
+
+            }
+            else{
+                l=mid+1;
+            }
+
+
+        }
+        return ans;
+
+
+
+
+
+    }
+
+    int upperBound(vector<int>&nums,int target){
+        int n=nums.size();
+        int l=0;
+        int r=n-1;
+        int ans=n;
+
+        while(l<=r){
+            int mid=(l+r)/2;
+
+            if(nums[mid]>target){
+                ans=mid;
+                r=mid-1;
+
+            }
+            else{
+                l=mid+1;
+            }
+
+
+        }
+        return ans;
+
+
+
+
+
+
+
+
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+            int lb=lowerBound(nums,target);
+            int ub=upperBound(nums,target);
+
+            if(lb==nums.size() || nums[lb]!=target)return {-1,-1};
+
+            return {lb,ub-1};
+
+
+    }
+};
+
+
