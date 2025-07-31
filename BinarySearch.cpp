3b41776bc34833e6b0610532fe2dc356ff7f4c52
Harshit Kharayat <harshitkharayat15@gmail.com>
2025-07-31 07:52:09 +0530

    540. Single Element in a Sorted Array (optimized approach)

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -365,3 +365,30 @@ public:
     }
 };
 
+// Binary search 
+//540. Single Element in a Sorted Array
+
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& arr) {
+        int n=arr.size();
+        int l=1;
+        int r=n-2;
+        if(n==1)return arr[0];
+        if(arr[0]!=arr[1])return arr[0];
+        if(arr[n-1]!=arr[n-2])return arr[n-1];
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1])return arr[mid];
+            // elimiate a serach space where element is not present 
+            if((mid%2==1 && arr[mid]==arr[mid-1])|| mid%2==0 && arr[mid]==arr[mid+1]){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return -1;
+    }
+};
