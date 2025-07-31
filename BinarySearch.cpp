66d0c555e36c52881408c74ea4124af302980914
Harshit Kharayat <harshitkharayat15@gmail.com>
2025-07-31 07:20:37 +0530

    540. Single Element in a Sorted Array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -341,3 +341,27 @@ int findKRotation(vector<int> &arr){
     
 }
 
+// Brute Force Approach for 540. Single Element in a Sorted Array
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return nums[0];
+
+        for(int i=0;i<n;i++){
+            if(i==0){
+                if(nums[i]!=nums[i+1])return nums[i];
+            }
+            else if(i==n-1){
+                if(nums[i]!=nums[i-1])return nums[i];
+            }
+            else{
+                if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1])
+                    return nums[i];
+            }
+
+
+        } return -1;   
+    }
+};
+
